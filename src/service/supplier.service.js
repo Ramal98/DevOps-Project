0be68f762e5415b8e1d8ee5f935ef50ada98fe65
haznymhd <145745688+haznymhd@users.noreply.github.com>
2024-02-22 20:30:03 +0530

    Update supplier.service.js

--- src/service/supplier.service.js
+++ src/service/supplier.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://3.210.203.241:8080/api/suppliers';
+const API_URL = 'http://3.238.78.78:8080/api/suppliers';
 
 class SupplierService {
   saveSupplier(supplier) {
